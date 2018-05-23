347455fbdf2ee26d958c379a2710d6c86d4e1e91
Ethan Brooks <ethanabrooks@gmail.com>
2018-05-23 18:58:43 -0400

    add epsilon to fps

--- sac/train.py
+++ sac/train.py
@@ -110,7 +110,7 @@ class Trainer:
                                 ]
                             }))
             s1 = s2
-            episode_mean.update(Counter(fps=1 / float(time.time() - tick)))
+            episode_mean.update(Counter(fps=1 / float(time.time() - tick) + 1e-10))
             episode_count.update(Counter(reward=r, timesteps=1))
             if t:
                 s1 = self.reset()

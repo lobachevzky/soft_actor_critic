be0eda103acbcce8be85e55e52248ad5e03f9f16
Ethan Brooks <ethanabrooks@gmail.com>
2018-06-03 12:49:14 -0400

    reset episode_mean along with episode_count

--- sac/train.py
+++ sac/train.py
@@ -130,7 +130,7 @@ class Trainer:
                     tb_writer.flush()
 
                 # zero out counters
-                episode_count = Counter()
+                episode_mean = episode_count = Counter()
 
     def build_agent(self, base_agent: AbstractAgent = AbstractAgent, **kwargs):
         state_shape = self.env.observation_space.shape

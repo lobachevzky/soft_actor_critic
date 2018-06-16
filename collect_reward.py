bf18f1593624765cc8b93ffe3f2dce63a313856e
Ethan Brooks <ethanabrooks@gmail.com>
2018-06-16 09:01:57 -0400

    improved collect reward to be more general

--- collect_reward.py
+++ collect_reward.py
@@ -3,16 +3,19 @@
 import tensorflow as tf
 from collections import deque
 from pathlib import Path
+import sys
+import argparse
 
-for run_dir in Path(".runs/tensorboard/5dof").iterdir():
-    events_file = next((f for f in run_dir.iterdir()
-                        if f.stem.startswith('events')), None)
-    if events_file:
-        q = deque(maxlen=20)
-        for event in tf.train.summary_iterator(str(events_file)):
-            reward = next((v.simple_value for v in event.summary.value
-                           if v.tag == 'reward'), None)
-            if reward is not None:
-                q += [reward]
-        with Path(run_dir, 'reward').open('w') as f:
-            f.write(str(sum(q) / float(len(q))))
+parser = argparse.ArgumentParser()
+parser.add_argument('path', type=Path)
+args = parser.parse_args()
+for path in args.path.glob('**/events*'):
+    print(path.parent)
+    q = deque(maxlen=20)
+    for event in tf.train.summary_iterator(str(path)):
+        reward = next((v.simple_value for v in event.summary.value
+                        if v.tag == 'reward'), None)
+        if reward is not None:
+            q += [reward]
+    with Path(path.parent, 'reward').open('w') as f:
+        f.write(str(sum(q) / float(len(q))))

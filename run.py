c6aa8d60c6fdbb638e6d78d36a896dd3a0aac48a
Ethan Brooks <ethanabrooks@gmail.com>
2018-06-22 19:06:23 -0400

    randomize block along x-axis

--- run.py
+++ run.py
@@ -4,22 +4,22 @@ import argparse
 from runs.main import main
 
 parser = argparse.ArgumentParser()
-parser.add_argument('forcerange', type=str)
+parser.add_argument('flags', nargs='*', type=str)
 args = parser.parse_args()
-r = args.forcerange
 command = "pick-and-place " \
           "--use-dof=slide_x " \
           "--use-dof=arm_flex_joint " \
           "--use-dof=wrist_roll_joint " \
           "--use-dof=hand_l_proximal_joint " \
           "--use-dof=hand_r_proximal_joint " \
-          f"--set-xml=\'actuator/position[@joint=\"slide_x\"]/forcerange\',\'-{r} {r}\'"
+          + ' '.join([' --' + f for f in args.flags])
+
 flags = [f'--flag={f}' for f in
          [
              '--set-xml='
+             '\'actuator/position[@joint=\"slide_x\"]/kp\',100|'
              '\'actuator/position[@joint=\"slide_x\"]/kp\',300|'
-             '\'actuator/position[@joint=\"slide_x\"]/kp\',500|'
-             '\'actuator/position[@joint=\"slide_x\"]/kp\',800',
+             '\'actuator/position[@joint=\"slide_x\"]/kp\',500',
              '--set-xml='
              '\'actuator/position[@joint=\"slide_x\"]/gear\',3|'
              '\'actuator/position[@joint=\"slide_x\"]/gear\',5|'
@@ -30,7 +30,7 @@ flags = [f'--flag={f}' for f in
              '\'body/joint[@name=\"slide_x\"]/damping\',2200',
          ]]
 main(['new',
-      f'4dof/forcerange={r}/',
+      f'4dof/block-movement/',
       command,
       '--description="search xml hyperparams to find good x_slide params"']
      + flags)

425bf043c52be8bd00b723a71609cf21932d4e85
Ethan Brooks <ethanabrooks@gmail.com>
2018-06-22 18:25:02 -0400

    init

--- run.py
+++ run.py
@@ -8,26 +8,26 @@ parser.add_argument('forcerange', type=str)
 args = parser.parse_args()
 r = args.forcerange
 command = "pick-and-place " \
-          "--use-dof=slide_x " \
+          "--use-dof=arm_lift_joint " \
           "--use-dof=arm_flex_joint " \
           "--use-dof=wrist_roll_joint " \
           "--use-dof=hand_l_proximal_joint " \
           "--use-dof=hand_r_proximal_joint " \
-          f"--set-xml=\'actuator/position[@joint=\"slide_x\"]/forcerange\',\'-{r} {r}\'"
+          f"--set-xml=\'actuator/position[@joint=\"arm_lift_joint\"]/forcerange\',\'-{r} {r}\'"
 flags = [f'--flag={f}' for f in
          [
              '--set-xml='
-             '\'actuator/position[@joint=\"slide_x\"]/kp\',300|'
-             '\'actuator/position[@joint=\"slide_x\"]/kp\',500|'
-             '\'actuator/position[@joint=\"slide_x\"]/kp\',800',
+             '\'actuator/position[@joint=\"arm_lift_joint\"]/kp\',5|'
+             '\'actuator/position[@joint=\"arm_lift_joint\"]/kp\',8|'
+             '\'actuator/position[@joint=\"arm_lift_joint\"]/kp\',10',
              '--set-xml='
-             '\'actuator/position[@joint=\"slide_x\"]/gear\',3|'
-             '\'actuator/position[@joint=\"slide_x\"]/gear\',5|'
-             '\'actuator/position[@joint=\"slide_x\"]/gear\',8',
+             '\'actuator/position[@joint=\"arm_lift_joint\"]/gear\',1|'
+             '\'actuator/position[@joint=\"arm_lift_joint\"]/gear\',3|'
+             '\'actuator/position[@joint=\"arm_lift_joint\"]/gear\',5',
              '--set-xml='
-             '\'body/joint[@name=\"slide_x\"]/damping\',1800|'
-             '\'body/joint[@name=\"slide_x\"]/damping\',2000|'
-             '\'body/joint[@name=\"slide_x\"]/damping\',2200',
+             '\'body/joint[@name=\"arm_lift_joint\"]/damping\',100|'
+             '\'body/joint[@name=\"arm_lift_joint\"]/damping\',150|'
+             '\'body/joint[@name=\"arm_lift_joint\"]/damping\',200',
          ]]
 main(['new',
       f'4dof/forcerange={r}/',

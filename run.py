0b1c09d59b4bba841f4f7ea9052b1f2768a3d9b0
Ethan Brooks <ethanabrooks@gmail.com>
2018-06-21 22:48:52 -0400

    added run.py

---
+++ run.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+import sys
+from pathlib import Path
+
+from runs.commands.new import cli
+from runs.util import RunPath
+
+command = "pick-and-place " \
+          "--use-dof=x_slide " \
+          "--use-dof=arm_flex_joint " \
+          "--use-dof=wrist_roll_joint " \
+          "--use-dof=hand_l_proximal_joint " \
+          "--use-dof=hand_r_proximal_joint "
+gear = sys.argv[1]
+cli(db_path=Path('runs.db'),
+    root=Path('.runs'),
+    dir_names=['tensorboard', 'checkpoints'],
+    quiet=False,
+    assume_yes=False,
+    path=RunPath('4dof/gear=' + gear),
+    prefix='setopt +o nomatch; EGL=1 nice',
+    command=command,
+    description='search xml hyperparams to find good x_slide params',
+    flags=[
+        f'--set-xml actuator/position[@name="slide_x"]/gear {gear}'
+        '--set-xml '
+        '\'actuator/position[@name="slide_x"]/k\' 3|'
+        '\'actuator/position[@name="slide_x"]/k\' 5|'
+        '\'actuator/position[@name="slide_x"]/k\' 7',
+        '--set-xml '
+        '\'actuator/position[@name="slide_x"]/forcerange\' "-100 100"|'
+        '\'actuator/position[@name="slide_x"]/forcerange\' "-200 200"|'
+        '\'actuator/position[@name="slide_x"]/forcerange\' "-300 300"',
+        '--set-xml '
+        '\'body/joint[@name="slide_x"]/damping\' 1500|'
+        '\'body/joint[@name="slide_x"]/damping\' 2000|'
+        '\'body/joint[@name="slide_x"]/damping\' 2500'
+    ])
+

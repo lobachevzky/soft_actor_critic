c6c77ae9031cbef8334924e00c837c38e5fca130
Ethan Brooks <ethanabrooks@gmail.com>
2018-07-16 15:24:08 -0400

    added frozen_lake script

---
+++ scripts/frozen_lake.py
@@ -0,0 +1,64 @@
+import click
+import tensorflow as tf
+from gym.wrappers import TimeLimit
+
+from environments.frozen_lake import FrozenLakeEnv
+from sac.networks import MlpAgent, MoEAgent
+from sac.train import Trainer
+
+
+def check_probability(ctx, param, value):
+    if not (0 <= value <= 1):
+        raise click.BadParameter("Param {} should be between 0 and 1".format(value))
+    return value
+
+
+@click.command()
+@click.option('--seed', default=0, type=int)
+@click.option('--n-layers', default=2, type=int)
+@click.option('--layer-size', default=128, type=int)
+@click.option('--learning-rate', default=8e-5, type=float)
+@click.option('--buffer-size', default=1e4, type=int)
+@click.option('--num-train-steps', default=1, type=int)
+@click.option('--batch-size', default=32, type=int)
+@click.option('--reward-scale', default=1e5, type=float)
+@click.option('--grad-clip', default=2e3, type=float)
+@click.option('--logdir', default=None, type=str)
+@click.option('--save-path', default=None, type=str)
+@click.option('--load-path', default=None, type=str)
+@click.option('--map-name', default="4x4", type=str)
+@click.option('--max-steps', default=100, type=int)
+@click.option('--render', is_flag=True)
+@click.option('--n-networks', default=None, type=int)
+def cli(seed, buffer_size, n_layers, layer_size, learning_rate, reward_scale, batch_size,
+        num_train_steps, logdir, save_path, load_path, render, grad_clip, map_name,
+        max_steps, n_networks):
+    env = TimeLimit(
+        env=FrozenLakeEnv(map_name=map_name, is_slippery=False),
+        max_episode_steps=max_steps)
+    kwargs = dict(
+        env=env,
+        seq_len=0,
+        device_num=1,
+        seed=seed,
+        buffer_size=buffer_size,
+        activation=tf.nn.relu,
+        n_layers=n_layers,
+        layer_size=layer_size,
+        learning_rate=learning_rate,
+        alpha=1. / reward_scale,
+        batch_size=batch_size,
+        grad_clip=grad_clip,
+        num_train_steps=num_train_steps,
+        logdir=logdir,
+        save_path=save_path,
+        load_path=load_path,
+        render=render)
+    if n_networks:
+        Trainer(base_agent=MoEAgent, n_networks=n_networks, **kwargs)
+    else:
+        Trainer(base_agent=MlpAgent, **kwargs)
+
+
+if __name__ == '__main__':
+    cli()

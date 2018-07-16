9f890e2ad0165b325d4e5ef2d4d53b909f6ffd55
Ethan Brooks <ethanabrooks@gmail.com>
2018-07-16 15:24:53 -0400

    update write_rewards

--- write_rewards.py
+++ write_rewards.py
@@ -11,6 +11,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', nargs='*', type=Path, default=[Path('.runs/tensorboard')])
     parser.add_argument('--smoothing', type=int, default=20)
+    parser.add_argument('--reward-file', default='reward')
     args = parser.parse_args()
     for path in (p for d in args.path for p in d.glob('**/events*')):
         print(path.parent)
@@ -20,13 +21,18 @@ def main():
         events = islice(iterator, max(length - n_reward, 0), length)
 
         def get_reward(event):
-            return next(
-                (v.simple_value for v in event.summary.value if v.tag == 'reward'), None)
+            return next((v.simple_value for v in event.summary.value
+                         if v.tag == args.reward_file), None)
 
         rewards = (get_reward(e) for e in events)
         rewards = [r for r in rewards if r is not None]
-        with Path(path.parent, 'reward').open('w') as f:
-            f.write(str(sum(rewards) / float(len(rewards))))
+        if rewards:
+            reward_path = Path(path.parent, args.reward_file)
+            print(f'Writing {reward_path}...')
+            with reward_path.open('w') as f:
+                f.write(str(sum(rewards) / float(len(rewards))))
+        else:
+            print('No rewards found')
 
 
 if __name__ == '__main__':

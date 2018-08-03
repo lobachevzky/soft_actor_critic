6c594f0f4aaed47b8531446d7d7e1e047fa31a6b
Ethan Brooks <ethanabrooks@gmail.com>
2018-08-03 14:11:21 -0400

    added multi_task_goals.py script

---
+++ scripts/multi_task_goals.py
@@ -0,0 +1,29 @@
+import itertools
+
+from gym import spaces
+import numpy as np
+import click
+
+
+def parse_coordinate(ctx, param, string):
+    if string is None:
+        return
+    return np.array(list(map(float, string.split(','))))
+
+
+@click.command()
+@click.option('--low', default='-.11,-.19,.412', callback=parse_coordinate)
+@click.option('--high', default='.08,.19,.412', callback=parse_coordinate)
+def cli(low, high):
+    goal_space = spaces.Box(low=low, high=high)
+    intervals = [2, 3, 1]
+    x, y, z = [
+        np.linspace(l, h, n)
+        for l, h, n in zip(goal_space.low, goal_space.high, intervals)
+    ]
+    goals = np.array(list(itertools.product(x, y, z)))
+    print('|'.join([','.join(map(str, np.round(g, 3))) for g in goals]))
+
+
+if __name__ == '__main__':
+    cli()

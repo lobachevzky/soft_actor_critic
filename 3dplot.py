e5c0a9115bb4b0eb4c7c481fc19d4def98176683
Ethan Brooks <ethanabrooks@gmail.com>
2018-08-10 14:37:38 -0400

    started plotting points in 3dplot

--- 3dplot.py
+++ 3dplot.py
@@ -9,28 +9,31 @@ def main():
     ax = fig.add_subplot(111, projection='3d')
 
     # goal-space=-.4to.4
-    xs = np.array([4e3, 4e3, 5e3, 6e3])
-    ys = np.array([4e-4, 2e-4, 2e-4, 2e-4])
-    zs = np.array([512, 512, 512, 512])
+    # rldl7: sort-runs .runs/tensorboard/multi-task-2d/goal_space=-.4to.4/ --smoothing=50
+    xs = np.array([3e3, 3e3, 6e3, 4e3, 5e3, 4e3])
+    ys = np.array([2e-4, 3e-4, 2e-4, 2e-4, 2e-4, 4e-4])
+    zs = np.array([512] * len(ys))
     ax.scatter(xs, ys, zs, c='r')
 
     # goal-space=-.2to.2
-    xs = np.array([3e3, 5e3, 1e4, 5e3, 5e3])
-    ys = np.array([2e-4, 2e-4, 1e-4, 1e-4, 1e-4])
-    zs = np.array([256, 256, 256, 256, 256])
+    # rldl7: sort-runs .runs/tensorboard/multi-task/geofence=.06/her-settings --smoothing=50
+    xs = np.array([5e3, 2e4, 1e4, 1e4, 1e4, 5e3, 1e4, 5e3, 5e3, 5e3, 5e3])
+    ys = np.array([2e-4, 5e-5, 5e-5, 5e-5, 1e-4, 2e-4, 1e-4, 5e-5, 1e-4, 1e-4, 1e-4])
+    zs = np.array([256] * len(ys))
     ax.scatter(xs, ys, zs, c='b')
 
-    # goal-space=-.2to.2
-    xs = np.array([3e3, 5e3, 1e4, 5e3, 5e3])
-    ys = np.array([2e-4, 2e-4, 1e-4, 1e-4, 1e-4])
-    zs = np.array([256, 256, 256, 256, 256])
-    ax.scatter(xs, ys, zs, c='b')
+    # fixed-shift=-.2to.2
+    # rldl12: sort-runs .runs/tensorboard/shift/geofence=.06/bumpers --smoothing=50
+    xs = np.array([1e4, 1e4, 1e4, 1e4, 1e4, 5e4])
+    ys = np.array([2e-4, 5e-5, 2e-4, 5e-5, 1e-4, 5e-5])
+    zs = np.array([256] * len(ys))
+    ax.scatter(xs, ys, zs, c='g', marker='x')
 
     ax.set_xlabel('reward scale')
     ax.set_ylabel('learning rate')
     ax.set_zlabel('layer size')
 
-    plt.save('best-params')
+    # plt.save('best-params')
     plt.show()
 
 

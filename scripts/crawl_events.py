cf1e9a39998e8614f34e9e761fa7802d35ec1041
Ethan Brooks <ethanabrooks@gmail.com>
2018-08-20 15:41:11 -0400

    update cache by default in crawl

--- scripts/crawl_events.py
+++ scripts/crawl_events.py
@@ -15,13 +15,13 @@ def main():
     parser.add_argument('--smoothing', type=int, default=2000)
     parser.add_argument('--tag', default='reward')
     parser.add_argument('--cache-file', default=None)
-    parser.add_argument('--update-cache', action='store_true')
+    parser.add_argument('--use-cache', action='store_true')
     args = parser.parse_args()
     data_points = crawl(dirs=args.dirs,
                         tag=args.tag,
                         smoothing=args.smoothing,
                         cache_file=args.cache_file or args.tag,
-                        update_cache=args.update_cache)
+                        use_cache=args.use_cache)
     print('Sorted lowest to highest:')
     print('*************************')
     for data, event_file in sorted(data_points):
@@ -32,7 +32,7 @@ DataPoint = namedtuple('DataPoint', 'data source')
 
 
 def crawl(dirs: List[Path], tag: str, smoothing: int, cache_file: Path,
-          update_cache: bool) -> List[DataPoint]:
+          use_cache: bool) -> List[DataPoint]:
     event_files = collect_events_files(dirs)
     data_points = []
     for event_file_path in event_files:
@@ -42,7 +42,7 @@ def crawl(dirs: List[Path], tag: str, smoothing: int, cache_file: Path,
         if data:
             cache_path = Path(event_file_path.parent, cache_file)
             data_points.append((data, event_file_path))
-            if update_cache or not cache_path.exists():
+            if not use_cache or not cache_path.exists():
                 print(f'Writing {cache_path}...')
                 with cache_path.open('w') as f:
                     f.write(str(data))

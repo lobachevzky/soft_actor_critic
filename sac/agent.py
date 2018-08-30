70d9db9b1058b31527780c625545f317617eb34e
Ethan Brooks <ethanabrooks@gmail.com>
2018-08-30 10:28:26 -0400

    use pi_network_log_prob in all places

--- sac/agent.py
+++ sac/agent.py
@@ -93,7 +93,7 @@ class AbstractAgent:
                     self.sample_pi_network('pi', reuse=True))
                 q2 = self.q_network(
                     self.O1, self.transform_action_sample(A_sampled2), 'Q', reuse=True)
-                log_pi_sampled2 = self.pi_network_log_prob(A_sampled2, 'pi', reuse=True)
+                log_pi_sampled2 = pi_network_log_prob(A_sampled2, 'pi', reuse=True)
                 self.pi_loss = pi_loss = tf.reduce_mean(
                     log_pi_sampled2 * tf.stop_gradient(log_pi_sampled2 - q2 + v1))
 

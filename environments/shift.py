c94f738d60c03da9c457a7d2631748682f477df9
Ethan Brooks <ethanabrooks@gmail.com>
2018-09-05 14:44:18 -0400

    rake out randomize_pose=randomize_pose

--- environments/shift.py
+++ environments/shift.py
@@ -23,7 +23,7 @@ class ShiftEnv(LiftEnv):
         self.geofence = geofence
         self.goal_space = spaces.Box(*map(np.array, zip(goal_x, goal_y)))
         self.goal = self.goal_space.sample() if fixed_goal is None else fixed_goal
-        super().__init__(fixed_block=False, randomize_pose=randomize_pose, **kwargs)
+        super().__init__(fixed_block=False, **kwargs)
         # low=np.array([-.14, -.22, .40]), high=np.array([.11, .22, .63]))
         # goal_size = np.array([.0317, .0635, .0234]) * geofence
         intervals = [2, 3, 1]

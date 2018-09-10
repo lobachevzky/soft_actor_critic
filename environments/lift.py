15bdbd95e1bb543679be1d43dfcd7239abb2b6c5
Ethan Brooks <ethanabrooks@gmail.com>
2018-09-10 13:38:49 -0400

    add new files

---
+++ environments/lift.py
@@ -0,0 +1,167 @@
+import random
+
+import numpy as np
+from gym import spaces
+
+from environments.mujoco import MujocoEnv
+from mujoco import ObjType
+from sac.utils import vectorize
+
+CHEAT_STARTS = [[
+    7.450e-05,
+    -3.027e-03,
+    4.385e-01,
+    1.000e+00,
+    0,
+    0,
+    -6.184e-04,
+    -1.101e+00,
+    0,
+    3.573e-01,
+    3.574e-01,
+], [-0.005, 0.025, 0.447, 0.488, -0.488, -0.512, 0.512, -1.101, 1.575, 0.357, 0.357], [
+    4.636e-03, 8.265e-06, 4.385e-01, 7.126e-01, 2.072e-17, -2.088e-17, -7.015e-01,
+    -1.101e+00, -1.575e+00, 3.573e-01, 3.574e-01
+], [
+    5.449e-03, -4.032e-03, 4.385e-01, 3.795e-01, 1.208e-17, -2.549e-17, -9.252e-01,
+    -1.101e+00, -7.793e-01, 3.573e-01, 3.574e-01
+]]
+
+
+def quaternion_multiply(quaternion1, quaternion0):
+    w0, x0, y0, z0 = quaternion0
+    w1, x1, y1, z1 = quaternion1
+    return np.array(
+        [
+            -x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0, x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
+            -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0, x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0
+        ],
+        dtype=np.float64)
+
+
+class LiftEnv(MujocoEnv):
+    def __init__(self,
+                 block_xrange=None,
+                 block_yrange=None,
+                 fixed_block=False,
+                 min_lift_height=.02,
+                 cheat_prob=0,
+                 obs_type='base-qvel',
+                 **kwargs):
+        if block_xrange is None:
+            block_xrange = (0, 0)
+        if block_yrange is None:
+            block_yrange = (0, 0)
+        self.block_xrange = block_xrange
+        self.block_yrange = block_yrange
+        self._obs_type = obs_type
+        self._cheated = False
+        self._cheat_prob = cheat_prob
+        self._fixed_block = fixed_block
+        self._block_name = 'block1'
+        self.min_lift_height = min_lift_height
+
+        super().__init__(**kwargs)
+
+        self.initial_qpos = np.copy(self.init_qpos)
+        self.initial_block_pos = np.copy(self.block_pos())
+        left_finger_name = 'hand_l_distal_link'
+        self._finger_names = [left_finger_name, left_finger_name.replace('_l_', '_r_')]
+        self.observation_space = spaces.Box(
+            low=-np.inf, high=np.inf, shape=np.shape(vectorize(self._get_obs())))
+
+        self.action_space = spaces.Box(
+            low=self.sim.actuator_ctrlrange[:-1, 0],
+            high=self.sim.actuator_ctrlrange[:-1, 1],
+            dtype=np.float32)
+        self._table_height = self.sim.get_body_xpos('pan')[2]
+        self._rotation_actuators = ["arm_flex_motor"]  # , "wrist_roll_motor"]
+        self.unwrapped = self
+
+    def _reset_qpos(self, qpos):
+        if np.random.uniform(0, 1) < self._cheat_prob:
+            self._cheated = True
+            qpos = np.array(random.choice(CHEAT_STARTS))
+        else:
+            self._cheated = False
+        if not self._fixed_block:
+            block_joint = self.sim.get_jnt_qposadr('block1joint')
+            qpos[block_joint + 0] = np.random.uniform(*self.block_xrange)
+            qpos[block_joint + 1] = np.random.uniform(*self.block_yrange)
+            qpos[block_joint + 3] = np.random.uniform(0, 1)
+            qpos[block_joint + 6] = np.random.uniform(-1, 1)
+
+        return qpos
+
+    def _get_obs_space(self):
+        qpos_limits = [(-np.inf, np.inf) for _ in self.sim.qpos]
+        qvel_limits = [(-np.inf, np.inf) for _ in self._qvel_obs()]
+        for joint_id in range(self.sim.njnt):
+            if self.sim.get_jnt_type(joint_id) in ['mjJNT_SLIDE', 'mjJNT_HINGE']:
+                qposadr = self.sim.get_jnt_qposadr(joint_id)
+                qpos_limits[qposadr] = self.sim.jnt_range[joint_id]
+        if not self._fixed_block:
+            block_joint = self.sim.get_jnt_qposadr('block1joint')
+            qpos_limits[block_joint:block_joint + 7] = [
+                self.block_xrange,  # x
+                self.block_yrange,  # y
+                (.4, .921),  # z
+                (0, 1),  # quat 0
+                (0, 0),  # quat 1
+                (0, 0),  # quat 2
+                (-1, 1),  # quat 3
+            ]
+        return spaces.Box(*map(np.array, zip(*qpos_limits + qvel_limits)))
+
+    def _qvel_obs(self):
+        def get_qvels(joints):
+            base_qvel = []
+            for joint in joints:
+                try:
+                    base_qvel.append(self.sim.get_joint_qvel(joint))
+                except RuntimeError:
+                    pass
+            return np.array(base_qvel)
+
+        return get_qvels(['slide_x', 'slide_x'])
+
+    def _get_obs(self):
+        return np.concatenate([self.sim.qpos, self._qvel_obs()])
+
+    def block_pos(self):
+        return self.sim.get_body_xpos(self._block_name)
+
+    def gripper_pos(self):
+        finger1, finger2 = [self.sim.get_body_xpos(name) for name in self._finger_names]
+        return (finger1 + finger2) / 2.
+
+    def _is_successful(self):
+        return self.block_pos()[2] > self.initial_block_pos[2] + self.min_lift_height
+
+    def compute_terminal(self):
+        return self._is_successful()
+
+    def compute_reward(self):
+        if self._is_successful():
+            return 1
+        else:
+            return 0
+
+    def step(self, action):
+        action = np.clip(action, self.action_space.low, self.action_space.high)
+
+        mirrored = 'hand_l_proximal_motor'
+        mirroring = 'hand_r_proximal_motor'
+
+        # insert mirrored values at the appropriate indexes
+        mirrored_index, mirroring_index = [
+            self.sim.name2id(ObjType.ACTUATOR, n) for n in [mirrored, mirroring]
+        ]
+        # necessary because np.insert can't append multiple values to end:
+        mirroring_index = np.minimum(mirroring_index, self.action_space.shape)
+        action = np.insert(action, mirroring_index, action[mirrored_index])
+
+        s, r, t, i = super().step(action)
+        if not self._cheated:
+            i['log count'] = {'successes': float(r > 0)}
+        return s, r, t, i
---
+++ scripts/plot.py
@@ -0,0 +1,166 @@
+#! /usr/bin/env python
+import argparse
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+# noinspection PyUnresolvedReferences
+from pandas.errors import EmptyDataError
+from sac.utils import softmax
+
+
+def parse_double(string, delimiter='x'):
+    if string is None:
+        return
+    a, b = map(float, string.split(delimiter))
+    return a, b
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csv_dir', type=Path)
+    parser.add_argument('--components', nargs='+')
+    parser.add_argument('--delimiter', default='|')
+    parser.add_argument('--plot-path', default=None)
+    parser.add_argument('--min-rows', type=int, default=15)
+    parser.add_argument('--weight-key')
+    parser.add_argument('--aggregate', action='store_true')
+    parser.add_argument('--fig-size', type=parse_double)
+    args = parser.parse_args()
+
+    plot(
+        labels=args.components,
+        delimiter=args.delimiter,
+        csv_dir=args.csv_dir,
+        plot_path=args.plot_path,
+        weight_key=args.weight_key,
+        min_rows=args.min_rows,
+        fig_size=args.fig_size,
+        aggregate=args.aggregate,
+    )
+
+    # # goal-space=-.4to.4
+    # # rldl7: sort-runs .runs/tensorboard/multi-task-2d/goal_space=-.4to.4/ --smoothing=50
+    # xs = np.array([3e3, 3e3, 6e3, 4e3, 5e3, 4e3])
+    # ys = np.array([2e-4, 3e-4, 2e-4, 2e-4, 2e-4, 4e-4])
+    # zs = np.array([512] * len(ys))
+    # ax.scatter(xs, ys, zs, c='r')
+    #
+    # # goal-space=-.2to.2
+    # # rldl7: sort-runs .runs/tensorboard/multi-task/geofence=.06/her-settings --smoothing=50
+    # xs = np.array([5e3, 2e4, 1e4, 1e4, 1e4, 5e3, 1e4, 5e3, 5e3, 5e3, 5e3])
+    # ys = np.array([2e-4, 5e-5, 5e-5, 5e-5, 1e-4, 2e-4, 1e-4, 5e-5, 1e-4, 1e-4, 1e-4])
+    # zs = np.array([256] * len(ys))
+    # ax.scatter(xs, ys, zs, c='b')
+    #
+    # # fixed-shift=-.2to.2
+    # # rldl12: sort-runs .runs/tensorboard/shift/geofence=.06/bumpers --smoothing=50
+    # xs = np.array([1e4, 1e4, 1e4, 1e4, 1e4, 5e4])
+    # ys = np.array([2e-4, 5e-5, 2e-4, 5e-5, 1e-4, 5e-5])
+    # zs = np.array([256] * len(ys))
+    # ax.scatter(xs, ys, zs, c='g', marker='x')
+
+
+def plot(csv_dir, delimiter, labels, plot_path, weight_key, min_rows, fig_size,
+         aggregate):
+    csv_paths = list(csv_dir.glob('*.csv'))
+    x_label, y_label, z_label, *legend_labels = labels
+    xyz_labels = [x_label, y_label, z_label]
+    fig = plt.figure(figsize=fig_size)
+    if weight_key:
+        nrows = ncols = 1
+    else:
+        nrows = int(len(csv_paths)**(1 / 2))
+        ncols = len(csv_paths) / nrows
+    ax = fig.add_subplot(nrows, ncols, 1, projection='3d')
+    i = 1
+    dfs = []
+    for csv_path in csv_paths:
+        print(f'Plotting {csv_path}')
+        try:
+            df = pd.read_csv(csv_path, delimiter=delimiter, index_col='path')
+        except EmptyDataError:
+            continue
+        if df.shape[0] < min_rows:
+            continue
+        dfs.append(df)
+
+    titles = csv_paths
+    if aggregate:
+        dfs = [pd.concat(dfs)]
+        titles = [None]
+
+    for df, title in zip(dfs, titles):
+
+        def filter_in_df(keys):
+            return [k for k in keys if k in df]
+
+        if weight_key:
+            scatter_weighted(
+                ax=ax,
+                weight_key=weight_key,
+                df=df[filter_in_df(labels + [weight_key])],
+                xyz_labels=xyz_labels)
+
+        else:
+            ax = fig.add_subplot(nrows, ncols, i, projection='3d')
+            scatter_unweighted(
+                ax=ax,
+                df=df[filter_in_df(labels)],
+                legend_labels=filter_in_df(legend_labels),
+                xyz_labels=xyz_labels)
+            i += 1
+
+        setters = [ax.set_xlabel, ax.set_ylabel, ax.set_zlabel]
+        for setter, xyz_label in zip(setters, xyz_labels):
+            setter(xyz_label)
+        if not weight_key:
+            ax.set_title(get_title(title), loc='left')
+            ax.legend(loc='upper right')
+
+    if plot_path is None:
+        plt.show()
+    else:
+        print('Saving...')
+        plt.savefig(plot_path)
+
+
+def get_title(csv_path):
+    try:
+        csv_path = csv_path.relative_to(Path.home())
+    except ValueError:
+        pass
+    return str(csv_path.stem)
+
+
+def format_label(k, v):
+    if .01 < abs(v) < 1000:
+        return '{}: {}'.format(k, v)
+    return '{}: {:.1e}'.format(k, v)
+
+
+def scatter_unweighted(ax, df, legend_labels, xyz_labels):
+    if legend_labels:
+        for label_values, group in df.groupby(legend_labels):
+            try:
+                label = ', '.join(
+                    format_label(k, v) for k, v in zip(legend_labels, label_values))
+            except TypeError:
+                label = format_label(legend_labels[0], label_values)
+
+            ax.scatter(*group[xyz_labels].values.T, label=label, s=200, alpha=.5)
+    else:
+        ax.scatter(*df[xyz_labels].values.T, s=200, alpha=.5)
+
+
+def scatter_weighted(ax, df, weight_key, xyz_labels):
+    weights = df[weight_key]
+    if weights.max() < .8:
+        return
+    softmaxed = softmax(weights, theta=100)
+    series = df.mul(softmaxed, axis=0).sum()
+    ax.scatter(*series[xyz_labels], c='b', s=500, alpha=.5)
+
+
+if __name__ == '__main__':
+    main()

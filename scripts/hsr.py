1cf212f065490deba31ed8d7f9dc9036d6403124
Ethan Brooks <ethanabrooks@gmail.com>
2018-11-03 20:13:54 -0400

    deleted unsupervised script and added unsupervised flag

--- scripts/hsr.py
+++ scripts/hsr.py
@@ -21,6 +21,7 @@ from environments.hindsight_wrapper import HSRHindsightWrapper, MBHSRHindsightWr
 from environments.hsr import HSREnv, MultiBlockHSREnv
 from sac.networks import MlpAgent
 from sac.train import HindsightTrainer, Trainer
+from sac.unsupervised_trainer import UnsupervisedTrainer
 
 
 def make_box(*tuples: Tuple[float, float]):
@@ -195,7 +196,8 @@ def main(max_steps, min_lift_height, geofence, hindsight_geofence, seed, buffer_
          goal_space, block_space, grad_clip, batch_size, n_train_steps,
          record_separate_episodes, steps_per_action, logdir, save_path, load_path, render,
          render_freq, n_goals, record, randomize_pose, image_dims, record_freq,
-         record_path, temp_path, save_threshold, no_random_reset, obs_type, multi_block):
+         record_path, temp_path, save_threshold, no_random_reset, obs_type,
+         multi_block, unsupervised):
     env = TimeLimit(
         max_episode_steps=max_steps,
         env=(MultiBlockHSREnv if multi_block else HSREnv)(
@@ -238,6 +240,8 @@ def main(max_steps, min_lift_height, geofence, hindsight_geofence, seed, buffer_
                 env=env, geofence=hindsight_geofence),
             n_goals=n_goals,
             **kwargs)
+    elif unsupervised:
+        trainer = UnsupervisedTrainer(env=env, **kwargs)
     else:
         trainer = Trainer(env=env, **kwargs)
     trainer.train(
@@ -292,6 +296,7 @@ def cli():
     p.add_argument('--set-xml', type=put_in_xml_setter, action='append', nargs='*')
     p.add_argument('--use-dof', type=str, action='append')
     p.add_argument('--multi-block', action='store_true')
+    p.add_argument('--unsupervised', action='store_true')
     main(**vars(p.parse_args()))
 
 

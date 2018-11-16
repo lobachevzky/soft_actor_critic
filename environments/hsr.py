c8cf7492936e098ae7cf991195b99e12a013cb5f
Ethan Brooks <ethanabrooks@gmail.com>
2018-11-16 12:44:24 -0500

    remove print1 statement

--- environments/hsr.py
+++ environments/hsr.py
@@ -346,9 +346,7 @@ class HSREnv:
             achieved_goal = self.block_pos()
         if desired_goal is None:
             desired_goal = self.goal
-        d = distance_between(achieved_goal, desired_goal)
-        print1(d)
-        return d < self.geofence
+        return distance_between(achieved_goal, desired_goal) < self.geofence
 
     def set_goal(self, goal: np.ndarray):
         # assert self.goal_space.contains(goal)
